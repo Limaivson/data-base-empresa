9a2b1181491acf2ebb2e2452273a5276d6ef8dd5
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-16 20:41:18 -0300

    feat-class-cliente

---
+++ data-base/Cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, id_cliente, nome, telefone, endereco):
+        self.id_cliente = id_cliente
+        self.nome = nome
+        self.telefone = telefone
+        self.endereco = endereco
